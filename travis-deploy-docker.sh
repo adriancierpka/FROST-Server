836888f2304678a40c65de121ed75c55f211a62b
Adrian <adrian.cierpka@iosb.fraunhofer.de>
2020-02-13 10:12:37 +0100

    new build command

--- travis-deploy-docker.sh
+++ travis-deploy-docker.sh
@@ -2,7 +2,8 @@
 set -e
 
 mvn install -DskipTests -Dmaven.javadoc.skip=true && mvn dockerfile:build -pl FROST-Server.HTTP,FROST-Server.MQTT,FROST-Server.MQTTP
-mvn dockerfile:tag@tag-version -pl FROST-Server.HTTP,FROST-Server.MQTT,FROST-Server.MQTTP
+#mvn dockerfile:tag@tag-version -pl FROST-Server.HTTP,FROST-Server.MQTT,FROST-Server.MQTTP
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t adriancierpka/frost-server:latest 
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
   mvn dockerfile:push@push-latest -Ddockerfile.useMavenSettingsForAuth=true -pl FROST-Server.HTTP,FROST-Server.MQTT,FROST-Server.MQTTP --settings travis-settings.xml
