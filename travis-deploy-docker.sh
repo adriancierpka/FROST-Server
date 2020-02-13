17da319ee7649c1656d1ba4720569beaa4a1915e
Adrian <adrian.cierpka@iosb.fraunhofer.de>
2020-02-13 11:24:38 +0100

    build02

--- travis-deploy-docker.sh
+++ travis-deploy-docker.sh
@@ -3,10 +3,14 @@ set -e
 
 mvn install -DskipTests -Dmaven.javadoc.skip=true #&& mvn dockerfile:build -pl FROST-Server.HTTP,FROST-Server.MQTT,FROST-Server.MQTTP
 #mvn dockerfile:tag@tag-version -pl FROST-Server.HTTP,FROST-Server.MQTT,FROST-Server.MQTTP
-
-cd FROST-Server.HTTP && docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t adriancierpka/frost-server-http:latest && cd ..
-cd FROST-Server.MQTT && docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t adriancierpka/frost-server-mqttp:latest && cd ..
-cd FROST-Server.MQTTP && docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t adriancierpka/frost-server:latest && cd ..
+printf "Start building images"
+ls
+cd FROST-Server.HTTP && docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t adriancierpka/frost-server-http:latest
+cd ..
+cd FROST-Server.MQTT && docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t adriancierpka/frost-server-mqttp:latest
+cd ..
+cd FROST-Server.MQTTP && docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t adriancierpka/frost-server:latest
+cd ..
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
   mvn dockerfile:push@push-latest -Ddockerfile.useMavenSettingsForAuth=true -pl FROST-Server.HTTP,FROST-Server.MQTT,FROST-Server.MQTTP --settings travis-settings.xml
